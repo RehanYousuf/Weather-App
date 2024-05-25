2a088786105c3391c77839169b6be51abaac1285
Rehan Yousuf <135339675+RehanYousuf@users.noreply.github.com>
2024-05-25 18:34:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ navigator.geolocation.getCurrentPosition((location) => {
             <div class="card" style="width: 22rem;">
             <div class="card-body" id="card">
               <h5 class="card-title">Weather App</h5>
-              <input type="search" id="serach" placeholder="Search City"><button id="serach-icon" onclick="changrCity()">🔍</button>
+              <input type="search" id="serach" placeholder="Search Cityf"><button id="serach-icon" onclick="changrCity()">🔍</button>
               <h1>${res.name}</h1>
               <h2>${res.sys.country}</h2>
               <div class="display">
               <div class="img-type">
-              <img id="pic" src="images/clear-day.svg" alt="images">
+              <img id="pic" src="./clear-day.svg" alt=".">
               </div>
               <div class="temp">
               <p>${Math.round(res.main.temp)}°C</p>
@@ -37,47 +37,47 @@ navigator.geolocation.getCurrentPosition((location) => {
             </div>
             `
             if(res.weather[0].main == "Clear day"){
-                document.getElementById("pic").src ="images/clear-day.svg"
+                document.getElementById("pic").src ="./clear-day.svg"
             }
             else if(res.weather[0].main == "Clouds"){
-                document.getElementById("pic").src = "images/clouds.png"
+                document.getElementById("pic").src = "./clouds.png"
                 document.getElementById("card").style.backgroundColor= "White";
             }
             else if(res.weather[0].main == "Cloudy"){
-                document.getElementById("pic").src = "images/cloudy.svg"
+                document.getElementById("pic").src = "./cloudy.svg"
             }
             else if(res.weather[0].main == "Drizzle"){
-                document.getElementById("pic").src = "images/drizzle.svg"
+                document.getElementById("pic").src = "./drizzle.svg"
             }
             else if(res.weather[0].main == "Dust"){
-                document.getElementById("pic").src = "images/dust.svg"
+                document.getElementById("pic").src = "./dust.svg"
             }
             else if(res.weather[0].main == "Fog"){
-                document.getElementById("pic").src = "images/fog.svg"
+                document.getElementById("pic").src = "./fog.svg"
             }
             else if(res.weather[0].main == "Haze"){
-                document.getElementById("pic").src = "images/haze.svg"
+                document.getElementById("pic").src = "./haze.svg"
             }
             else if(res.weather[0].main == "Mist"){
-                document.getElementById("pic").src = "images/mist.svg"
+                document.getElementById("pic").src = "./mist.svg"
             }
             else if(res.weather[0].main == "Rain"){
-                document.getElementById("pic").src = "images/rainy-1.svg"
+                document.getElementById("pic").src = "./rainy-1.svg"
             }
             else if(res.weather[0].main == "Smoke"){
-                document.getElementById("pic").src = "images/smoke.svg"
+                document.getElementById("pic").src = "./smoke.svg"
             }
             else if(res.weather[0].main == "Snow"){
-                document.getElementById("pic").src = "images/snow.svg"
+                document.getElementById("pic").src = "./snow.svg"
             }
             else if(res.weather[0].main == "Thunderstorm"){
-                document.getElementById("pic").src = "images/thunderstorms.svg"
+                document.getElementById("pic").src = "./thunderstorms.svg"
             }
             else if(res.weather[0].main == "Tornado"){
-                document.getElementById("pic").src = "images/tornado.svg"
+                document.getElementById("pic").src = "./tornado.svg"
             }
             else if(res.weather[0].main == "Partly cloudy"){
-                document.getElementById("pic").src = "images/partly cloudy.png"
+                document.getElementById("pic").src = "./partly cloudy.png"
             }
 
         })
@@ -102,7 +102,7 @@ function changrCity(){
               <h2>${res.sys.country}</h2>
               <div class="display">
               <div class="img-type">
-              <img id="pic" src="images/clear-day.svg" alt="images">
+              <img id="pic" src="./clear-day.svg" alt=".">
               </div>
               <div class="temp">
               <p>${Math.round(res.main.temp)}°C</p>
@@ -125,59 +125,59 @@ function changrCity(){
             </div>
             `
              if(res.weather[0].main == "Clear"){
-                document.getElementById("pic").src ="images/clear-day.svg"
+                document.getElementById("pic").src ="./clear-day.svg"
                 document.getElementById("card").style.backgroundColor= "#ffd480";
             }
             else if(res.weather[0].main == "Clouds"){
-                document.getElementById("pic").src = "images/clouds.png"
+                document.getElementById("pic").src = "./clouds.png"
                 document.getElementById("card").style.backgroundColor= "#f7f8f3";
             }
             else if(res.weather[0].main == "Cloudy"){
-                document.getElementById("pic").src = "images/cloudy.svg"
+                document.getElementById("pic").src = "./cloudy.svg"
                 document.getElementById("card").style.backgroundColor= "lightgrey";
             }
             else if(res.weather[0].main == "Drizzle"){
-                document.getElementById("pic").src = "images/drizzle.svg"
+                document.getElementById("pic").src = "./drizzle.svg"
                 document.getElementById("card").style.backgroundColor= "#94acb6";
             }
             else if(res.weather[0].main == "Dust"){
-                document.getElementById("pic").src = "images/dust.svg"
+                document.getElementById("pic").src = "./dust.svg"
                 document.getElementById("card").style.backgroundColor= "#e7d3b7";
             }
             else if(res.weather[0].main == "Fog"){
-                document.getElementById("pic").src = "images/fog.svg"
+                document.getElementById("pic").src = "./fog.svg"
                 document.getElementById("card").style.backgroundColor= "#a0a0a0";
             }
             else if(res.weather[0].main == "Haze"){
-                document.getElementById("pic").src = "images/haze.svg"
+                document.getElementById("pic").src = "./haze.svg"
                 document.getElementById("card").style.backgroundColor= "#d6d7d9";
             }
             else if(res.weather[0].main == "Mist"){
-                document.getElementById("pic").src = "images/mist.svg"
+                document.getElementById("pic").src = "./mist.svg"
                 document.getElementById("card").style.backgroundColor= "#d6d7d9";
             }
             else if(res.weather[0].main == "Rain"){
-                document.getElementById("pic").src = "images/rainy-1.svg"
+                document.getElementById("pic").src = "./rainy-1.svg"
                 document.getElementById("card").style.backgroundColor= "lightblue";
             }
             else if(res.weather[0].main == "Smoke"){
-                document.getElementById("pic").src = "images/smoke.svg"
+                document.getElementById("pic").src = "./smoke.svg"
                 document.getElementById("card").style.backgroundColor= "lightgrey";
             }
             else if(res.weather[0].main == "Snowfall"){
-                document.getElementById("pic").src = "images/snow.svg"
+                document.getElementById("pic").src = "./snow.svg"
                 document.getElementById("card").style.backgroundColor= "white";
             }
             else if(res.weather[0].main == "Thunderstorm"){
-                document.getElementById("pic").src = "images/thunderstorms.svg"
+                document.getElementById("pic").src = "./thunderstorms.svg"
                 document.getElementById("card").style.backgroundColor= "lightblue";
             }
             else if(res.weather[0].main == "Tornado"){
-                document.getElementById("pic").src = "images/tornado.svg"
+                document.getElementById("pic").src = "./tornado.svg"
                 document.getElementById("card").style.backgroundColor= "grey";
             }
             else if(res.weather[0].main == "Partly cloudy"){
-                document.getElementById("pic").src = "images/partly cloudy.png"
+                document.getElementById("pic").src = "./partly cloudy.png"
                 document.getElementById("card").style.backgroundColor= "lightskyblue";
             }
             // weather.value="";
